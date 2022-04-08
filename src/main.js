f7a2f2dfbba05d5003c6509608d490e13749fabb
chen-jm <chenjmcjm@foxmail.com>
2022-04-09 01:46:56 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import VueCookies from 'vue-cookies'
+import ElementUI from 'element-ui'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
+Vue.use(VueCookies)
+Vue.use(ElementUI)
 
 new Vue({
   router,
